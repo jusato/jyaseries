e026e1ecf5d87a19c039ad3761ffa67128111e0a
Júlia Sato <juliasato13@gmail.com>
2020-08-03 13:49:13 -0300

    update home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import React, { useEffect, useState } from 'react';
 // import dadosIniciais from '../../data/dados_iniciais.json';
 import BannerMain from '../../components/BannerMain';
